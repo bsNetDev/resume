cf2d2526dcf59676bd53aed11146483101be0bff
Bryan Sessions <bryan.sessions@gmail.com>
2018-03-22 15:29:32 -0400

    commit

---
+++ dist/main.bundle.js
@@ -0,0 +1,165 @@
+webpackJsonp([2,5],{
+
+/***/ 141:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(46)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 146:
+/***/ (function(module, exports) {
+
+module.exports = "<nav class=\"navbar navbar-expand-lg navbar-dark bg-primary fixed-top\" id=\"sideNav\">\n  <a class=\"navbar-brand js-scroll-trigger\" href=\"#page-top\">\n    <span class=\"d-block d-lg-none\">Start Bootstrap</span>\n    <span class=\"d-none d-lg-block\">\n      <img class=\"img-fluid img-profile rounded-circle mx-auto mb-2\" src=\"img/profile.jpg\" alt=\"\">\n    </span>\n  </a>\n  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n    <span class=\"navbar-toggler-icon\"></span>\n  </button>\n  <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n    <ul class=\"navbar-nav\">\n      <li class=\"nav-item\">\n        <a class=\"nav-link js-scroll-trigger\" href=\"#about\">About</a>\n      </li>\n      <li class=\"nav-item\">\n        <a class=\"nav-link js-scroll-trigger\" href=\"#experience\">Experience</a>\n      </li>\n      <li class=\"nav-item\">\n        <a class=\"nav-link js-scroll-trigger\" href=\"#education\">Education</a>\n      </li>\n      <li class=\"nav-item\">\n        <a class=\"nav-link js-scroll-trigger\" href=\"#skills\">Skills</a>\n      </li>\n      <li class=\"nav-item\">\n        <a class=\"nav-link js-scroll-trigger\" href=\"#interests\">Interests</a>\n      </li>\n      <li class=\"nav-item\">\n        <a class=\"nav-link js-scroll-trigger\" href=\"#awards\">Awards</a>\n      </li>\n    </ul>\n  </div>\n</nav>\n\n<div class=\"container-fluid p-0\">\n\n  <section class=\"resume-section p-3 p-lg-5 d-flex d-column\" id=\"about\">\n    <div class=\"my-auto\">\n      <h1 class=\"mb-0\">Clarence\n        <span class=\"text-primary\">Taylor</span>\n      </h1>\n      <div class=\"subheading mb-5\">3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·\n        <a href=\"mailto:name@email.com\">name@email.com</a>\n      </div>\n      <p class=\"mb-5\">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>\n      <ul class=\"list-inline list-social-icons mb-0\">\n        <li class=\"list-inline-item\">\n          <a href=\"#\">\n            <span class=\"fa-stack fa-lg\">\n              <i class=\"fa fa-circle fa-stack-2x\"></i>\n              <i class=\"fa fa-facebook fa-stack-1x fa-inverse\"></i>\n            </span>\n          </a>\n        </li>\n        <li class=\"list-inline-item\">\n          <a href=\"#\">\n            <span class=\"fa-stack fa-lg\">\n              <i class=\"fa fa-circle fa-stack-2x\"></i>\n              <i class=\"fa fa-twitter fa-stack-1x fa-inverse\"></i>\n            </span>\n          </a>\n        </li>\n        <li class=\"list-inline-item\">\n          <a href=\"#\">\n            <span class=\"fa-stack fa-lg\">\n              <i class=\"fa fa-circle fa-stack-2x\"></i>\n              <i class=\"fa fa-linkedin fa-stack-1x fa-inverse\"></i>\n            </span>\n          </a>\n        </li>\n        <li class=\"list-inline-item\">\n          <a href=\"#\">\n            <span class=\"fa-stack fa-lg\">\n              <i class=\"fa fa-circle fa-stack-2x\"></i>\n              <i class=\"fa fa-github fa-stack-1x fa-inverse\"></i>\n            </span>\n          </a>\n        </li>\n      </ul>\n    </div>\n  </section>\n\n  <section class=\"resume-section p-3 p-lg-5 d-flex flex-column\" id=\"experience\">\n    <div class=\"my-auto\">\n      <h2 class=\"mb-5\">Experience</h2>\n\n      <div class=\"resume-item d-flex flex-column flex-md-row mb-5\">\n        <div class=\"resume-content mr-auto\">\n          <h3 class=\"mb-0\">Senior Web Developer</h3>\n          <div class=\"subheading mb-3\">Intelitec Solutions</div>\n          <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>\n        </div>\n        <div class=\"resume-date text-md-right\">\n          <span class=\"text-primary\">March 2013 - Present</span>\n        </div>\n      </div>\n\n      <div class=\"resume-item d-flex flex-column flex-md-row mb-5\">\n        <div class=\"resume-content mr-auto\">\n          <h3 class=\"mb-0\">Web Developer</h3>\n          <div class=\"subheading mb-3\">Intelitec Solutions</div>\n          <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>\n        </div>\n        <div class=\"resume-date text-md-right\">\n          <span class=\"text-primary\">December 2011 - March 2013</span>\n        </div>\n      </div>\n\n      <div class=\"resume-item d-flex flex-column flex-md-row mb-5\">\n        <div class=\"resume-content mr-auto\">\n          <h3 class=\"mb-0\">Junior Web Designer</h3>\n          <div class=\"subheading mb-3\">Shout! Media Productions</div>\n          <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>\n        </div>\n        <div class=\"resume-date text-md-right\">\n          <span class=\"text-primary\">July 2010 - December 2011</span>\n        </div>\n      </div>\n\n      <div class=\"resume-item d-flex flex-column flex-md-row\">\n        <div class=\"resume-content mr-auto\">\n          <h3 class=\"mb-0\">Web Design Intern</h3>\n          <div class=\"subheading mb-3\">Shout! Media Productions</div>\n          <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>\n        </div>\n        <div class=\"resume-date text-md-right\">\n          <span class=\"text-primary\">September 2008 - June 2010</span>\n        </div>\n      </div>\n\n    </div>\n\n  </section>\n\n  <section class=\"resume-section p-3 p-lg-5 d-flex flex-column\" id=\"education\">\n    <div class=\"my-auto\">\n      <h2 class=\"mb-5\">Education</h2>\n\n      <div class=\"resume-item d-flex flex-column flex-md-row mb-5\">\n        <div class=\"resume-content mr-auto\">\n          <h3 class=\"mb-0\">University of Colorado Boulder</h3>\n          <div class=\"subheading mb-3\">Bachelor of Science</div>\n          <div>Computer Science - Web Development Track</div>\n          <p>GPA: 3.23</p>\n        </div>\n        <div class=\"resume-date text-md-right\">\n          <span class=\"text-primary\">August 2006 - May 2010</span>\n        </div>\n      </div>\n\n      <div class=\"resume-item d-flex flex-column flex-md-row\">\n        <div class=\"resume-content mr-auto\">\n          <h3 class=\"mb-0\">James Buchanan High School</h3>\n          <div class=\"subheading mb-3\">Technology Magnet Program</div>\n          <p>GPA: 3.56</p>\n        </div>\n        <div class=\"resume-date text-md-right\">\n          <span class=\"text-primary\">August 2002 - May 2006</span>\n        </div>\n      </div>\n\n    </div>\n  </section>\n\n  <section class=\"resume-section p-3 p-lg-5 d-flex flex-column\" id=\"skills\">\n    <div class=\"my-auto\">\n      <h2 class=\"mb-5\">Skills</h2>\n\n      <div class=\"subheading mb-3\">Programming Languages &amp; Tools</div>\n      <ul class=\"list-inline list-icons\">\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-html5\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-css3\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-javascript\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-jquery\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-sass\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-less\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-bootstrap\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-wordpress\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-grunt\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-gulp\"></i>\n        </li>\n        <li class=\"list-inline-item\">\n          <i class=\"devicons devicons-npm\"></i>\n        </li>\n      </ul>\n\n      <div class=\"subheading mb-3\">Workflow</div>\n      <ul class=\"fa-ul mb-0\">\n        <li>\n          <i class=\"fa-li fa fa-check\"></i>\n          Mobile-First, Responsive Design</li>\n        <li>\n          <i class=\"fa-li fa fa-check\"></i>\n          Cross Browser Testing &amp; Debugging</li>\n        <li>\n          <i class=\"fa-li fa fa-check\"></i>\n          Cross Functional Teams</li>\n        <li>\n          <i class=\"fa-li fa fa-check\"></i>\n          Agile Development &amp; Scrum</li>\n      </ul>\n    </div>\n  </section>\n\n  <section class=\"resume-section p-3 p-lg-5 d-flex flex-column\" id=\"interests\">\n    <div class=\"my-auto\">\n      <h2 class=\"mb-5\">Interests</h2>\n      <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skiier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>\n      <p class=\"mb-0\">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technolgy advancements in the front-end web development world.</p>\n    </div>\n  </section>\n\n  <section class=\"resume-section p-3 p-lg-5 d-flex flex-column\" id=\"awards\">\n    <div class=\"my-auto\">\n      <h2 class=\"mb-5\">Awards &amp; Certifications</h2>\n      <ul class=\"fa-ul mb-0\">\n        <li>\n          <i class=\"fa-li fa fa-trophy text-warning\"></i>\n          Google Analytics Certified Developer</li>\n        <li>\n          <i class=\"fa-li fa fa-trophy text-warning\"></i>\n          Mobile Web Specialist - Google Certification</li>\n        <li>\n          <i class=\"fa-li fa fa-trophy text-warning\"></i>\n          1<sup>st</sup>\n          Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>\n        <li>\n          <i class=\"fa-li fa fa-trophy text-warning\"></i>\n          1<sup>st</sup>\n          Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>\n        <li>\n          <i class=\"fa-li fa fa-trophy text-warning\"></i>\n          2<sup>nd</sup>\n          Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>\n        <li>\n        <li>\n          <i class=\"fa-li fa fa-trophy text-warning\"></i>\n          1<sup>st</sup>\n          Place - James Buchanan High School - Hackathon 2006</li>\n        <li>\n          <i class=\"fa-li fa fa-trophy text-warning\"></i>\n          3<sup>rd</sup>\n          Place - James Buchanan High School - Hackathon 2005</li>\n      </ul>\n    </div>\n  </section>\n\n</div>\n"
+
+/***/ }),
+
+/***/ 177:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(74);
+
+
+/***/ }),
+
+/***/ 73:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 73;
+
+
+/***/ }),
+
+/***/ 74:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(84);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__(86);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(87);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 85:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(6);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__(146),
+        styles: [__webpack_require__(141)]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 86:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(22);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(82);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(83);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(85);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 87:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ })
+
+},[177]);
+//# sourceMappingURL=main.bundle.js.map
---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  // Instruct the app
+  // to use the forceSSL
+  // middleware
+  app.use(forceSSL());
+
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+
+// Start the app by listening on the default
+// Heroku port
+let port = process.env.PORT || 8080;
+console.log(`App is listening on port: ${port}`);
+app.listen(port);
+
